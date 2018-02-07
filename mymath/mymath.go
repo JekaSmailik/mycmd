35d348cc860b690a334581cfda51a00821f16b1e
Evgeniy <89181813770@mail.ru>
2018-02-07 10:33:39 +0300

    добавлена история команд и ее чистка

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -1,15 +1,20 @@
 package mymath
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
+// Путь места нахождения программы
+var path = "../mycmd/mymath/historyFile.txt"
+
 func MyTime() string {
 	timeNow := time.Now()
-	mytime := timeNow.Format("Дата 2.01.2006" +
-		"\nВремя 15:04\n")
+	mytime := timeNow.Format("Дата 02.01.2006\nВремя 15:04\n")
 
 	var myday string
 
@@ -88,7 +93,7 @@ func MyMassiv(nam int) []int {
 
 			fmt.Println(massiv)
 		}
-	// Перебор массива и присвоение ему данных от 0 до длины массива.
+		// Перебор массива и присвоение ему данных от 0 до длины массива.
 	default:
 		fmt.Println("В какой последоватальности вы хотите заполнить массив? \n" +
 			"если по возростанию введите '+', если на убывание то '-' \n" +
@@ -111,6 +116,93 @@ func MyMassiv(nam int) []int {
 			massiv = append(massiv)
 		}
 	}
-
 	return massiv
+}
+
+// Сохранение всех вводов командной строки в файле по ссылке
+func MyWrite(x string) {
+	// открытие файла с возможностью добавления информации
+	historyFile, err := os.OpenFile(path, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0777)
+
+	if err != nil {
+		// здесь перехватывается ошибка
+		// создание нового файла
+		historyFile, err = os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
+	}
+	defer historyFile.Close()
+
+	// Добавление к выводу команд нумерацию в порядке возростания
+	var countLine int
+	f := bufio.NewReader(historyFile)
+	for {
+		_, err := f.ReadString('\n')
+		countLine++
+		if err != nil {
+			break
+		}
+	}
+
+	// Запись информации в файл
+	byteSlice := []byte(strconv.FormatInt(int64(countLine), 10) + " :" + x + "\n")
+	_, err = historyFile.Write(byteSlice)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//	n, err := historyFile.WriteString(x)
+	//	fmt.Println(err, n)
+}
+
+// Выводит историю вводимых команд
+func MyHistory() {
+
+	fmt.Println("Ниже предоставленна история вводимых команд")
+	//fmt.Println("Общее колличество введенных команд", len())
+
+	historyFile, err := os.Open(path)
+	if err != nil {
+		// здесь перехватывается ошибка
+		fmt.Println(err)
+
+		return
+	}
+	defer historyFile.Close()
+
+	// Получение размера файла
+	stat, err := historyFile.Stat()
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	// Чтение файла
+	readFile := make([]byte, stat.Size())
+	_, err = historyFile.Read(readFile)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+	// Вывод данных файла
+	speakFile := string(readFile)
+	fmt.Println(speakFile)
+
+	/*
+		strings.NewReader(path)
+			fmt.Printf("%v", historyFile)
+	*/
+}
+
+// Очищает файл по ссылке path
+func MyClear() {
+	err := os.Remove(path)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
